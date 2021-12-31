e2900f1f4227ddc804406259b08feae076f0487d
Chuck Michelson <chuckmichelson@gmail.com>
2021-12-31 11:02:46 -0600

    testing

--- server/utils.js
+++ server/utils.js
@@ -4,8 +4,8 @@ module.exports = {
 
 function makeid(length) {
    var result           = '';
-   // var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-   var characters       = 'A';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   // var characters       = 'A';
    var charactersLength = characters.length;
    for ( var i = 0; i < length; i++ ) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
