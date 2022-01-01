1de35b2055e3a94092c13173bb0ab5f8a8907258
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-01 01:54:54 -0600

    testing

--- server/game.js
+++ server/game.js
@@ -55,7 +55,7 @@ function gameLoop(state) {
   }
 
   score = state.players[0].snake.length
-  document.getElementById('scoreDisplay').innerText = String(score)
+  // document.getElementById('scoreDisplay').innerText = String(score)
 
   const playerOne = state.players[0];
   const playerTwo = state.players[1];
