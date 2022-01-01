ef5aa33edee22daf029b0e69541143193253bc64
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-01 01:35:45 -0600

    testing

--- server/game.js
+++ server/game.js
@@ -76,7 +76,7 @@ function gameLoop(state) {
   if (state.food.x === playerOne.pos.x && state.food.y === playerOne.pos.y) {
     playerOne.snake.push({ ...playerOne.pos });
     state.score = state.players[0].snake.length
-    document.getElementById('scoreDisplay') = state.score
+    document.getElementById('scoreDisplay').innerText = state.score
     playerOne.pos.x += playerOne.vel.x;
     playerOne.pos.y += playerOne.vel.y;
     randomFood(state);
