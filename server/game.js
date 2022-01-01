f6ef390fbe8433b5dada4d658ce8216789529b61
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-01 10:19:34 -0600

    testing

--- server/game.js
+++ server/game.js
@@ -106,11 +106,11 @@ function gameLoop(state) {
   }
 
   if (playerTwo.vel.x || playerTwo.vel.y) {
-    for (let cell of playerTwo.snake) {
-      // if (cell.x === playerTwo.pos.x && cell.y === playerTwo.pos.y) {
-      //   // return 1;
-      // }
-    }
+    // for (let cell of playerTwo.snake) {
+    //   // if (cell.x === playerTwo.pos.x && cell.y === playerTwo.pos.y) {
+    //   //   // return 1;
+    //   // }
+    // }
 
     playerOne.snake.push({ ...playerOne.pos });
     playerOne.snake.shift();
