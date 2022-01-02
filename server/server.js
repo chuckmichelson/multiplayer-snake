34ae613eaa3001385783bcecf3e6bd4de4c60154
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-02 12:15:50 -0600

    testing

--- server/server.js
+++ server/server.js
@@ -15,6 +15,8 @@ io.on('connection', client => {gameLoop
   function handleJoinGame(roomName) {
     console.log("made it to handleJoinGame")
     const room = io.sockets.adapter.rooms[roomName];
+    console.log("Room:")
+    console.log(room)
 
     let allUsers;
     if (room) {
