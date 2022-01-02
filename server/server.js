ae7ceb196bb4250ccf2ace445b931ef4f99e02f1
Chuck Michelson <chuckmichelson@gmail.com>
2022-01-02 11:49:06 -0600

    testing

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ io.on('connection', client => {gameLoop
     }
 
     if (numClients === 0) {
-      client.emit('unknownCode');
+      //client.emit('unknownCode');
       handleNewGame()
       return;
     }
